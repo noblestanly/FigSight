041a43e63fdc1b6966227488482ff6cef6dc9965
noblestanly <noblestanly28@gmail.com>
2025-09-04 10:59:55 +0530

    Added sql database

--- backend/settings.py
+++ backend/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'figsight_db',
-        'USER': 'postgres',          # your DB user
-        'PASSWORD': '2812',  # the password you set during install
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'figsight_db',
+#         'USER': 'postgres',          # your DB user
+#         'PASSWORD': '2812',  # the password you set during install
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
